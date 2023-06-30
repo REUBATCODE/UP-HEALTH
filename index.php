9440ec7b376054248ef00106b68a0f71681cf761
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2023-06-30 00:18:58 -0700

    Creación de UI de la pagina de bienvenida Sencilla

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>UpHealth</title>
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor contenido-header"> 
+                <div class="barra">
+                    <a href="./" class="logo">
+                        <img src="./img/uphealth-logo-nobg.png" alt="logo de la pagina">
+                    </a>
+                    <nav class="nav">
+                        <a href="./">HOME</a>
+                        <a href="./">Registrarse</a>
+                        <a href="./">Iniciar Session</a>
+                    </nav>
+                </div>
+        </div>
+    </header>
+    <main class="contenedor">
+        <h1>Inicia tu entrenamiento</h1>
+        <p>Desde La comodidad de tu casa</p>
+        <section class="subscribete">
+                
+            <div class="subscribete-comprar">   
+                <a href="./">
+                    <p>Empieza Ya!!</p>
+                </a>
+            </div>
+        </section>
+    </main>
+    <footer class="footer">
+        <div class="footer-copyright">
+            <p>Derechos reservados UpHealth</p>
+        </div>
+    </footer>
+</body>
+</html>
