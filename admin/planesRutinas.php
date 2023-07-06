f26f29527388f87a1942b4307957f240e8a3ccdd
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-07-05 19:08:38 -0700

    cambio de diagnoles
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- admin/planesRutinas.php
+++ admin/planesRutinas.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="..//css/style.css">
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-    <link rel="stylesheet" href="..//css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="planesRutinas.html">
 
