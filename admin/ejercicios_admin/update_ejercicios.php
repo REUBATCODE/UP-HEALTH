9e5a1edc95ddaa9e8f13f4faeaad27eb6c480bb5
Reub Vega <odiseocowboy2022@gmail.com>
2023-08-10 21:42:02 -0700

    Cambios en el diseño de las pantallas, diseño unificado en el crud

--- admin/ejercicios_admin/update_ejercicios.php
+++ admin/ejercicios_admin/update_ejercicios.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../../css/crud-admin.css">
+    <link rel="stylesheet" href="../../css/createAdmin.css">
     <title>Editar Rutina</title>
 </head>
 <body>
--- admin/users-admin/crear.php
+++ admin/users-admin/crear.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../../css/crud-admin.css">
-    <title>crear administrador</title>
+    <link rel="stylesheet" href="../../css/createAdmin.css">
+    <title>Crear Administrador</title>
 </head>
 <body>
     <main>
