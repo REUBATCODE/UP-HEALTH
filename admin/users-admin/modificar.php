091515535830f902d6a0c7c5cd51c80aaf5b8789
Reub Vega <odiseocowboy2022@gmail.com>
2023-08-10 22:29:48 -0700

    Diseño de modificar

--- admin/users-admin/modificar.php
+++ admin/users-admin/modificar.php
@@ -119,7 +119,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./../../css/crud-admin.css">
+    <link rel="stylesheet" href="../../css/createAdmin.css">
     <title>crear administrador</title>
 </head>
 <body>
